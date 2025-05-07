949b80d8cb70e1a840ef1abffaad758ede8f5faf
Gauthier Claudel <gauthier.claudel@etu.unistra.fr>
2025-05-07 11:03:01 +0200

    Dice constructeur

--- src/Dice/Dice.java
+++ src/Dice/Dice.java
@@ -1,4 +1,18 @@
 package Dice;
 
 public class Dice {
+    private final int m_value;
+    private final int m_nbThrow;
+
+    public Dice(int trhow,int value){
+        m_nbThrow =trhow;
+        m_value = value;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return m_nbThrow+"d"+m_value;
+    }
 }

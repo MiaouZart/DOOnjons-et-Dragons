e84697e77324ce7df1f6088cd5645ea0479fda5b
Gauthier Claudel <gauthier.claudel@etu.unistra.fr>
2025-05-07 10:45:50 +0200

    Add testRace

---
+++ tests/Race/RaceTest.java
@@ -0,0 +1,42 @@
+package Race;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaceTest {
+
+    @org.junit.jupiter.api.Test
+    void testHuman() {
+        Human human = new Human();
+        assertEquals(2,human.getSpeed());
+        assertEquals(2,human.getDex());
+        assertEquals(2,human.getInitiative());
+        assertEquals(2,human.getStrenght());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDwarf() {
+        Dwarf dwarf = new Dwarf();
+        assertEquals(0,dwarf.getSpeed());
+        assertEquals(0,dwarf.getDex());
+        assertEquals(0,dwarf.getInitiative());
+        assertEquals(6,dwarf.getStrenght());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testElf(){
+        Elf elf = new Elf();
+        assertEquals(0,elf.getSpeed());
+        assertEquals(6,elf.getDex());
+        assertEquals(0,elf.getInitiative());
+        assertEquals(0,elf.getStrenght());
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHalfelin() {
+        Halfelin halfelin = new Halfelin();
+        assertEquals(0,halfelin.getSpeed());
+        assertEquals(6,halfelin.getDex());
+        assertEquals(0,halfelin.getInitiative());
+        assertEquals(0,halfelin.getStrenght());
+    }
+}

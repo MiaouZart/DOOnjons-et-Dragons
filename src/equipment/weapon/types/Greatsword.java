b95cb5264431bcb7c577b1c0479eedfff7bd09b7
Guillaume Simon <guillaume.simon4@etu.unistra.fr>
2025-05-19 10:56:17 +0200

    Ajout de l'épée à deux mains

---
+++ src/equipment/weapon/types/Greatsword.java
@@ -0,0 +1,10 @@
+package equipment.weapon.types;
+
+import dice.Dice;
+import equipment.weapon.Martial;
+
+public class Greatsword extends Martial {
+    public Greatsword() {
+        super("  Épée à Deux Mains", 1, new Dice(2,6));
+    }
+}
